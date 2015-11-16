5110e38b1a28edf9944d68576b2f9b1e4cc1d0ae
Chris Caragianis <chriscaragianis@gmail.com>
2015-11-15 23:35:33 -0500

    Add test class

---
+++ FizzBuzzTest.java
@@ -0,0 +1,7 @@
+class FizzBuzzTest {
+  //FizzBuzz test class
+  //A program to test the functionality of our FizzBuzz class
+  public static void main(String[] args) {
+    //Program logic goes here
+  }
+}
