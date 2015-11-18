8533c5c5b58b316b1c9d8c8ec4533d54cdf919c7
houyouheng <houyouheng@hotmail.com>
2015-11-18 11:04:42 -0500

    outline with comments in FizzBuzzTest.java

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -3,7 +3,26 @@
 class FizzBuzzTest {
   //FizzBuzz test class
   //A program to test the functionality of our FizzBuzz class
-  public static void main(String[] args) {
-    //Program logic goes here
-  }
+	public static void fizzNumberTest() {
+		//n=0
+		//n=1
+		//n=3
+		//n=5
+		//n=15
+		//n="string"
+		//n="-9"
+		//n="-15"
+		//n="-10"
+	}
+	public static void main (String [] arg){
+
+	}
+	public static void fizzListTest(){
+		//start n=1 end=5
+		//start n=12 end=16
+		//start n=-2 end=2
+		//start n=10 end=5
+
+	}
 }
+
